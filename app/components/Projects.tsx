18087cdcd52caa5ff1157eb4e93de2671f43473d
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-11-09 15:17:55 +0530

    added some projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -10,11 +10,28 @@ import parel from "@/public/img/projects/parel.png";
 import ibm from "@/public/img/projects/ibm.png";
 import gpt3 from "@/public/img/projects/gpt3.png";
 import metaversus from "@/public/img/projects/MacBook Pro - 2.png";
+import ecommerce from "@/public/img/projects/ecommerce-nextjs-sreerag-k-p.vercel.app_.png";
+import youtube from "@/public/img/projects/youtube.png";
 const Projects = () => {
   return (
     <div className="wrapper">
       <Title text="Projects" icon={<FaProjectDiagram />} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
+        <ProjectCard
+          title="Ecommerce website"
+          link="https://ecommerce-nextjs-sreerag-k-p.vercel.app/"
+          img={ecommerce.src}
+        />
+        <ProjectCard
+          title="Youtube-clone"
+          link="https://youtube-next-n98iv2ga6-sreerag-k-p.vercel.app"
+          img={youtube.src}
+        />
+        <ProjectCard
+          title="Bookstore-mern"
+          link="https://book-store-mern-stack-s5oc-eb4b26epu-sreerag-k-p.vercel.app/"
+          img={bookstore.src}
+        />
         <ProjectCard
           title="Parel creative -landing page clone"
           link="https://sreerag-k-p.github.io/Procreativeclonesite/"
@@ -46,16 +63,11 @@ const Projects = () => {
           img={metaversus.src}
         />
         <ProjectCard title="Portfolio" link="#" img={portfolio.src} />
-        <ProjectCard
+        {/* <ProjectCard
           title="Threads"
           link="https://threads-app-tau-sooty.vercel.app/"
           img={threads.src}
-        />
-        <ProjectCard
-          title="Bookstore-mern"
-          link="https://book-store-mern-stack-s5oc-eb4b26epu-sreerag-k-p.vercel.app/"
-          img={bookstore.src}
-        />
+        /> */}
       </div>
     </div>
   );
